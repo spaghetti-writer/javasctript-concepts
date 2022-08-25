7023f5b6766c07b41714353fa21144fb80c72bf5
Williams <williamsalvaradovs1520@hotmail.com>
2022-08-24 19:06:46 -0500

    ciclos basicos

--- arrays.js
+++ arrays.js
@@ -36,13 +36,29 @@ console.log(_array.join()); // convierte a string, (mutacion no permanente)
 console.log(_array); // le da la vuelta al array
 console.log(_array.reverse()); // le da la vuelta al array (mutacion permanente)
 
-/*splice
+/*splice (mutacion permanente)
 	(a,b,items) - cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos
 	a - Indice de inicio
 	b - Numero de elementos (opcional)
 	items - elementos a añadir en caso de que se deba añadir (opcional)
+	- si b vale 0 agrega los elemmentos a partir de la posicioon a y no elimina ninguno
 */
 
 // current array [-3,-2,-1,0,2,3,5,8,9,11]
 console.log(_array.splice(2)); // op: [-1,0,2,3,5,8,9,11]
-console.log(_array);
+console.log(_array); // op: [-3,-2]
+
+let _array_2 = [1,2,3,4,5,6,7,8,9]
+console.log(_array_2.splice(6,2,10,11,12)); // op: [7,8]
+console.log(_array_2); // op: [1,2,3,4,5,6,10,11,12,9]
+
+/* slice(a,b) (mutacion no permanente)
+	extrae los elementos desde el indice a hasta b
+		- si b no existe lo extrae hasta el final, 
+		- si a y b no existen hace una copia hasta el completa
+*/
+
+let _array_3 = _array_2.slice();
+console.log(_array_3);
+console.log(_array_3.slice(4,7)); // op: 5,6, 10
+console.log(_array_3);
---
+++ bucle_for.js
@@ -0,0 +1,21 @@
+// Bucles indeterminados (while(){}, do{}while())
+// Bucles determinados (for)
+let numeros = [1,2,3,4,5,6,7,8];
+let deep = numeros.length;
+let string = 'Hola mundo';
+
+//for (let i = 0; i < deep; i++) {
+//	console.log(numeros[i]);
+//}
+
+// for (n of numeros) {
+// 	console.log(n);
+// }
+
+// for(let i=0;i<string.length;i++){
+// 	console.log(string[i]);
+// }
+
+for(l of string){
+	console.log(l);
+}
